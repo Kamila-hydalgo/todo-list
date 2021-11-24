4b5ec8e047b7dd8a8549a780387496c4ef3ad129
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-11-23 21:44:13 -0300

    Requisito 7 e 8 feitos

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Requisito 5.
+// Requisito 5 e 6.
 // Referência: projeto criado na monitoria do Thalles.
-const addButtom = document.getElementById('criar-tarefa');
+const addButtom = document.querySelector('#criar-tarefa');
 addButtom.addEventListener('click', function () {
   const input = document.querySelector('#texto-tarefa');
   const inputValue = input.value;
-
   const taskList = document.querySelector('#lista-tarefas');
   const taskName = document.createElement('li');
   taskName.innerText = inputValue;
+  taskName.addEventListener('click', addRemove)
   taskList.appendChild(taskName);
   input.value = '';
 });
+
+// Requisito 7.
+function addRemove(event) {
+  const taskName = event.target;
+  const taskSelected = document.querySelector('.selected');
+  if (taskSelected) {
+    taskSelected.classList.remove('selected');
+  }
+  taskName.classList.add('selected');
+}
